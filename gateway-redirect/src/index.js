35e7ae52e540c3086f2a253a15b8cac61cd28d53
Alec M. Wantoch <alec@wantoch.com>
2022-08-07 14:27:44 -0400

    IPFS Gateway Redirect

---
+++ gateway-redirect/src/index.js
@@ -0,0 +1,18 @@
+/// <reference types="@fastly/js-compute" />
+import { CID } from 'multiformats/cid';
+
+addEventListener("fetch", (event) => event.respondWith(handleRequest(event)));
+
+const CID_REGEX = new RegExp(/^\/ipfs\/(Qm[1-9A-HJ-NP-Za-km-z]{44,}|b[A-Za-z2-7]{58,}|B[A-Z2-7]{58,}|z[1-9A-HJ-NP-Za-km-z]{48,}|F[0-9A-F]{50,})/);
+
+async function handleRequest(event) {
+  const reqURL = new URL(event.request.url);
+  try {
+    const match = reqURL.pathname.match(CID_REGEX)[1];
+    const cid = CID.parse(match);
+    const newURL = new URL(`https://${cid.toV1().toString()}.ipfs.gateway-new.valist.io${reqURL.pathname.replace(CID_REGEX, '')}`);
+    return new Response(null, { headers: new Headers({ location: newURL.toString() }), status: 302, url: newURL.toString() });
+  } catch (e) {
+    return new Response(`invalid cid :c ${reqURL.toString()}`);
+  }
+}
